470aa970e1c434986910b2848bacffe7d57c6f34
pns1681999 <43614413+pns1681999@users.noreply.github.com>
2020-06-29 01:43:44 +0700

    comment and delete post

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,6 +7,7 @@ const Post = mongoose.model("Post");
 router.get('/allpost',requireLogin, (req, res) => {
     Post.find()
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy","_id name")
     .then(posts=>{
         res.json({posts})
     })
@@ -39,6 +40,7 @@ router.post('/createpost',requireLogin, (req, res)=>{
 router.get('/mypost',requireLogin, (req, res) => {
     Post.find({postedBy:req.user._id})
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy","_id name")
     .then(myposts=>{
         res.json({myposts})
     })
@@ -52,7 +54,9 @@ router.put('/like', requireLogin, (req,res)=>{
         $push:{likes:req.user._id}
     },{
         new:true
-    }).exec((err, result)=>{
+    }).populate("comments.postedBy","_id name")
+    .populate("postedBy","_id name")
+    .exec((err, result)=>{
         if(err){
             return res.status(422).json({error:err})
         }else{
@@ -66,7 +70,9 @@ router.put('/unlike', requireLogin, (req,res)=>{
         $pull:{likes:req.user._id}
     },{
         new:true
-    }).exec((err, result)=>{
+    }).populate("comments.postedBy","_id name")
+    .populate("postedBy","_id name")
+    .exec((err, result)=>{
         if(err){
             return res.status(422).json({error:err})
         }else{
@@ -75,4 +81,43 @@ router.put('/unlike', requireLogin, (req,res)=>{
     })
 })
 
+router.put('/comment', requireLogin, (req,res)=>{
+    const comment = {
+        text:req.body.text,
+        postedBy: req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{comments:comment}
+    },{
+        new:true
+    }).populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.delete('/deletepost/:postId',requireLogin, (req, res)=>{
+    Post.findOne({_id:req.params.postId})
+    .populate("postedBy", "_id")
+    .exec((err,post)=>{
+        if(err||!post){
+            return res.status(422).json({error:err})
+        }
+
+        if(post.postedBy._id.toString() === req.user._id.toString()){
+            post.remove()
+            .then(result=>{
+                res.json(result)
+            }).catch(err=>{
+                console.log(err)
+            })
+        }
+        
+    })
+})
 module.exports = router
